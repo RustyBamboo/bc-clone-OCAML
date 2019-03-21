3a915eee159bbc04243a6a51529e5b6cdee6789d
grymestone <grymestone@ufl.edu>
2019-03-20 23:06:19 -0400

    FUNCTION: Add Function Support, Change Format to Accomidate

--- bc.ml
+++ bc.ml
@@ -1,9 +1,9 @@
 open Core
-
+(* Input expression or list of expressions *)
 type sExpr = 
     | Atom of string
     | List of sExpr list
-
+(* Top level expression dealing with functions, operators, variables numbers *)
 type expr = 
     | Num of float
     | Var of string
@@ -11,6 +11,8 @@ type expr =
     | Op2 of string*expr*expr
     | Fct of string * expr list
 
+
+(* Statement handles our key words that have blocks or require special handling *)
 type statement = 
     | Assign of string*expr
     | Return of expr
@@ -18,42 +20,52 @@ type statement =
     | If of expr*statement list * statement list
     | While of expr*statement list
     | For of statement*expr*statement*statement list
-    | FctDef of string * string list * statement list 
+    | FctDef of string * expr list * statement list 
 
+(* A block of code that requires its own environment for local variables *)
 type block = statement list 
 
+(* Environment to store variables in *)
 type env = (string*float) list
 
-        (* complete *)
+(* List of Functions *)
+type fctEnv = (string * expr list * statement list  ) list
 
+(* Queue of Environments to handle nested functions/recursion*)
 type envQueue = env list
 
-let rec printTupleList l = match l with
+(* Prints anyt list passed into it it would seem. From head to tail. Expects each index to have two variables. *)
+let printTupleList l = match l with
     | [] -> ()
     | h::t -> let (x,y) = h in
             printf "%s %f, " x y
+
+(* Prints the environment queue *)
 let rec printQueue que = match que with
     | [] -> ()
     | h::t -> printTupleList h; printf "\n" ; printQueue t
 
-
+(* Finds a variable? What does the rec keyword do? It means its Recursive...*)
 let rec find_var var l =
     match l with
             [] -> raise Not_found
             |(s, i)::tl -> if s = var then i
                                   else find_var var tl
 
+(* Evaluates a variable *)
 let varEval (_v: string) (_q:envQueue): float  =
     match List.hd _q with
     | Some h -> find_var _v h
-    | None -> failwith "Not found"
+    | None -> failwith "Not found Results Variable"
 
+(* Evaluate operators for ++ and -- *)
 let eval_op1 op (e1: float): float=
     match op with 
   | "++" -> e1 +. 1.0
   | "--" -> e1 -. 1.0
   | _ -> failwith "oof"
 
+(* Evaluate simple math operators*)
 let eval_op2 op e1 e2 =
     match op with
   | "+" -> (+.) e1 e2
@@ -63,74 +75,118 @@ let eval_op2 op e1 e2 =
   | ">" -> if (e1 > e2) then 1.0 else 0.0
   | x -> printf "%s" x; (+.) e1 e2
 
-let rec evalExpr (_e: expr) (_q:envQueue): float  = 
-    match _e with
-    | Num n -> n
-    | Var v -> varEval v _q
-    | Op1 (op, e1) -> (eval_op1 op (evalExpr e1 _q))
-    | Op2 (op, e1, e2) -> (eval_op2 op (evalExpr e1 _q) (evalExpr e2 _q))
-    | Fct _ -> 1.2
+let rec find_fct var p l =
+    match l with
+        [] -> raise Not_found
+        |(s, pl, bl)::tl -> if s = var & List.length p = List.length pl then bl
+                                else find_fct var p tl
 
-(* Test for expression *)
-let%expect_test "evalNum" = 
-    evalExpr (Op1("++", Num 1.0)) [] |>
-    printf "%F";
-    [%expect {| 10. |}]
+let rec evalFct (_n:string) (_p: float list) (_f:fctEnv) : block =
+    match List.hd _f with
+    | Some h -> find_fct _n _p _f
+    | None -> failwith "Not found Funcky boi"
 
+
+(* Inserts a number at the end?  *)
 let rec insert_at_end l i =
   match l with
     [] -> [i]
   | h :: t -> h :: (insert_at_end t i)
 
+(* This just finds the head? *)
 let hd l = match List.hd l with
     | Some x -> x
     | None -> failwith "oof"
 
-let rec evalCode (_code: block) (_q:envQueue): unit = 
+(* Insert into function list *)
+let decFct f fe =
+    match f with
+        [] -> [fe]
+    | h :: t -> h :: (insert_at_end t fe)
+
+(* Evaluates a block of code using the environment queue, returns result of the code block. First place the input is sent *)
+let rec evalCode (_code: block) (_q:envQueue) (_f:fctEnv): float = 
     let q = [] @ _q in 
     printQueue q;
+    (* let f = [] @ _f in
+    printFunc f; *)
     match _code with 
-    | [] -> ();
-    | h:: t -> let outq = evalStatement h q in
-                evalCode t outq; ()
+    | [] -> 1.2;
+    | h::t -> let out = evalStatement h q _f in
+              let (outf, outq) = out in
+              evalCode t outq outf
     (* let y = List.fold_left evalStatement  [] in ()*)
     (* let x = evalStatement (hd _code) q in () *)
     (* in let (a, b) = hd (hd x) in *)
     (* printf "%s %f\n" a b *)
     (* pop the local environment *)
+    (* let y = List.fold_left evalStatem *)
 
-
-and evalStatement (s: statement) (q:envQueue): envQueue =
+(* UwU whats this? a floating and statement?
+It appears to take in a statement and environment queue and creates a new environment for the statement. 
+And allows you to call evalCode and allows evalCode to call evalStatement. Recursively. Mutually recursive types.*)
+and evalStatement (s: statement) (q:envQueue) (f:fctEnv): (fctEnv*envQueue) =
     match s with 
-        | Assign(_v, _e) -> let out = evalExpr _e q in
+        | Assign(_v, _e) -> let out = evalExpr _e q f in
                             let sc = List.hd q in
-                            insert_at_end q [(_v, out)]
+                            let q = insert_at_end q [(_v, out)]
+                            in (f, q)
         | If(e, codeT, codeF) -> 
-            let cond = evalExpr e q in
+            let cond = evalExpr e q f in
                 if(cond>0.0) then
-                    evalCode codeT q 
+                    let _ = evalCode codeT q f in
+                    (f,q)
                 else
-                    evalCode codeF q
-            ;q
-        | Expr e -> let out = evalExpr e q in
-                printf "%f\n" out; q
-        | For (s1, e, s2, code) -> q (* ree *)
-        | _ -> q (*ignore *)
+                    let _ = evalCode codeF q f in
+                    (f, q)
+        | Expr e -> let out = evalExpr e q f in
+                printf "%f\n" out; (f, q)
+        | For (s1, e, s2, code) -> (f, q) (* ree *)
+        | FctDef (n, p, bl) -> let fe = decFct f (n, p, bl) in
+                            (fe, q)
+        | _ -> (f, q) (*ignore *)
 
+(* Evaluates expressions, matching it with an associated type. *)
+and evalExpr (_e: expr) (_q:envQueue) (_f:fctEnv): float  = 
+    match _e with
+    | Num n -> n
+    | Var v -> varEval v _q
+    | Op1 (op, e1) -> (eval_op1 op (evalExpr e1 _q _f))
+    | Op2 (op, e1, e2) -> (eval_op2 op (evalExpr e1 _q _f) (evalExpr e2 _q _f))
+    | Fct (n, p) -> evalCode (evalFct n (evalExprList p [] _q _f) _f) _q _f 
+    | _ -> 1.2
+
+and evalExprList (p: expr list) (r: float list) (_q:envQueue) (_f:fctEnv): float list =
+    match p with 
+    | [] -> [];
+    | h::t -> let e = evalExpr h _q _f in 
+                    r@[e]
+
+(* Test for expression *)
+let%expect_test "evalNum" = 
+    evalExpr (Op1("++", Num 1.0)) [] [] |>
+    printf "%F";
+    [%expect {| 2. |}]
+
+
+(* p999... what a descriptive name. This is a test of the assignment operator. *)
 let p999: statement = Assign("v", Num(1.0))
-let%test _ = evalStatement p999 [] = [[("v", 1.0)]]
-
+let%test _ = evalStatement p999 [] [] = ([], [[("v", 1.0)]])
 (* 
     v = 10; 
     v // display v
  *)
+ (* Test assignment function. We assign V to the number 1, then just pass v in to see if it works. 
+ What we should see in our output is v 1.0, 
+ 1.0
+ Whereby v 1.0 is when it gets set and 1.0 being when v is evaluated.  *)
 let p1: block = [
         Assign("v", Num(1.0));
         Expr(Var("v")) 
 ]
 
 let%expect_test "p1" =
-    evalCode p1 []; 
+    let _ = evalCode p1 [] [] in (); 
     [%expect {| 1. |}]
 
 (*
@@ -160,8 +216,8 @@ let p2: block = [
     Expr(Var("v"))
 ]
 
-let%expect_test "p1" =
-    evalCode p2 []; 
+let%expect_test "p2" =
+    let _ = evalCode p2 [] [] in (); 
     [%expect {| 3628800. |}]
 
 (*  Fibbonaci sequence
@@ -177,13 +233,13 @@ let%expect_test "p1" =
  *)
 let p3: block = 
     [
-        FctDef("f", ["x"], [
+        FctDef("f", [Var("x")], [
             If(
                 Op2("<", Var("x"), Num(1.0)),
                 [Return(Num(1.0))],
                 [Return(Op2("+",
                     Fct("f", [Op2("-", Var("x"), Num(1.0))]),
-                    Fct("f", [Op2("-", Var("x"), Num(1.0))])
+                    Fct("f", [Op2("-", Var("x"), Num(2.0))])
                 ))])
         ]);
         Expr(Fct("f", [Num(3.0)]));
@@ -191,7 +247,7 @@ let p3: block =
     ]
 
 let%expect_test "p3" =
-    evalCode p3 []; 
+    let _ = evalCode p3 [] [] in (); 
     [%expect {| 
         2. 
         5.      
